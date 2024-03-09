01df56c40c0603e21f6e63f5581f69b281601dc6
AdityacodeNIT <srivastavaditya145@gmail.com>
2024-03-09 15:32:19 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,69 +1,93 @@
-import './App.css';
-import Navbar from './component/Navbar';
-import TextForm from './component/TextForm' ;
-import About from './component/About';
-import Alert from './component/Alert';
-import ReactDOM from 'react-dom';
-import { useState } from 'react';
-import Contact from './component/Contact'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
-function App() {
+import "./App.css";
+import React from "react";
+import Navbar from "./component/Navbar";
+import TextForm from "./component/TextForm";
+import About from "./component/About";
+import Alert from "./component/Alert";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-  const[alert,SetAlert]=useState(null);
-  const[backgroundColo,setBackgroundColo]=useState('light');
-  const showAlert=(message,type)=>{
+import { useState } from "react";
+import Contact from "./component/Contact";
+
+function App() {
+  const [alert, SetAlert] = useState(null);
+  const [backgroundColo, setBackgroundColo] = useState("light");
+
+  const showAlert = (message, type) => {
     SetAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
       SetAlert(null);
-    },1700);
-  }
-  const removeBodyClasses=()=>{
-    document.body.classList.remove('bg-light')
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-success')
-    document.body.classList.remove('bg-primary')
-    document.body.classList.remove('bg-light')
-  }
+    }, 1700);
+  };
+  const removeBodyClasses = () => {
+    document.body.classList.remove("bg-light");
+    document.body.classList.remove("bg-dark");
+    document.body.classList.remove("bg-warning");
+    document.body.classList.remove("bg-success");
+    document.body.classList.remove("bg-primary");
+    document.body.classList.remove("bg-light");
+  };
   const toggleMode = (cls) => {
     removeBodyClasses();
-    document.body.classList.add('bg-' + cls);
-    showAlert(`${cls} mode has been enabled`, 'success');
+    document.body.classList.add("bg-" + cls);
+    showAlert(`${cls} mode has been enabled`, "success");
   };
-    const toggleColor=()=>{
-      if(backgroundColo==='light'){
-        setBackgroundColo('dark');
-        document.body.style.backgroundColor='#032845';
-        showAlert("dark mode has been enabled", "success")
-        }
-        else{
-          setBackgroundColo('light');
-          document.body.style.backgroundColor='white';
-          showAlert("light mode has been enabled", "success")
-        }
-     }
-  return (
-    <> 
- <Navbar title="Parmarth" backgroundColo={backgroundColo}  toggleMode={toggleMode} toggleColor={toggleColor}/>
- 
- <Alert alert={alert} backgroundColo={backgroundColo}/>
- 
- <div className="container my-3">
-  <BrowserRouter>
-  <Routes>
-          <Route path="/About" element={<About  backgroundColo={backgroundColo}  showAlert={showAlert} toggleColor={toggleColor}/>}/>
-          <Route path="/" element={<TextForm backgroundColo={backgroundColo} showAlert={showAlert} toggleColor={toggleColor} />} />
-          <Route path="/Contact" element={<Contact/>}/>  
-          </Routes>
-    </BrowserRouter>
- </div>
+  const toggleColor = () => {
+    if (backgroundColo === "light") {
+      setBackgroundColo("dark");
+      document.body.style.backgroundColor = "#032845";
 
-</>   
+      showAlert("dark mode has been enabled", "success");
+    } else {
+      setBackgroundColo("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Navbar
+        title="Parmarth"
+        backgroundColo={backgroundColo}
+        toggleMode={toggleMode}
+        toggleColor={toggleColor}
+      />
+
+      <Alert alert={alert} backgroundColo={backgroundColo} />
+
+      <div className="container my-3">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/About"
+              element={
+                <About
+                  backgroundColo={backgroundColo}
+                  showAlert={showAlert}
+                  toggleColor={toggleColor}
+                />
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  backgroundColo={backgroundColo}
+                  toggleColor={toggleColor}
+                />
+              }
+            />
+            <Route path="/Contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
-}  
+}
+
 export default App;
